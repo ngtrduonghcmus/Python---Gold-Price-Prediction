e56c8851fd482f75bcb1adf26a76200e7b0b12af
Nguyễn Triều Dương <145764634+ngtrduonghcmus@users.noreply.github.com>
2025-12-09 15:00:08 +0700

    Refactor main.py for data processing and cleanup
    Refactor data processing steps and clean up code.

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ from preprocessing.data_loader import DataOverview
 from preprocessing.eda import StatisticsAnalyzer, Visualizer
 from preprocessing.preprocessor import DataPreprocessor, DataScaler
 
-# Đọc dữ liệu
-import gdown
-
-file_id = "10h8D0SEASQmL0AORY6okEP6HSHbgqggn"
-url = f"https://drive.google.com/uc?id={file_id}"
-
-# Tải file về Colab
-output = "test.csv"
-gdown.download(url, output, quiet=False)
-
-
 FILE_PATH = "test.csv"
 # Đọc và Thiết lập DataFrame gốc
 print("--- 1. ĐỌC DỮ LIỆU ---")
@@ -27,13 +16,17 @@ preprocessor = DataPreprocessor(df=overview.df, encoders=overview.encoders)
 scaler = DataScaler(df=overview.df, scalers=overview.scalers)
 visualizer = Visualizer(df=overview.df)
 
+"""#THỐNG KÊ MÔ TẢ"""
+
 # Phân tích Thống kê
 print("Thống kê mô tả:")
 analyzer.get_describe_stats()
 
-# # Báo cáo Trực quan tự động (Chạy Visualizer)
+# Báo cáo Trực quan tự động (Chạy Visualizer)
 # print(" Báo cáo Trực quan Tự động:")
 # visualizer.full_report()
+
+"""#TIỀN XỬ LÝ DỮ LIỆU"""
 
 # Gọi hàm để làm sạch và chuyển đổi cột 'Date'
 df=preprocessor.clean_date_column(column_name='Date')
@@ -61,7 +54,14 @@ df=preprocessor.clean_numeric_format()
 # Kiểm tra số NaN cho tất cả các cột
 nan_counts = df.isna().sum()
 nan_counts = nan_counts[nan_counts > 0]
-print(nan_counts)
+nan_counts
+
+"""- **Các cột có giá trị NaN** (High, SP_Ajclose, DJ_low, EG_open, EG_high, EG_low, EU_Price, EU_high, OF_Price, OS_Price):  
+  - Số lượng NaN nhỏ (1-2 giá trị mỗi cột).  
+  - Các chỉ số này biến động **khá nhỏ trong một giai đoạn ngắn**, nên thay NaN bằng **phương pháp nội suy 10 giá trị xung quang**.  
+  - Không nên thay bằng trung bình toàn bộ cột, vì dữ liệu trải dài theo thời gian và biến động lớn theo giai đoạn dài.  
+  - Cách làm này giữ được **tính liên tục cục bộ** và phản ánh sát giá trị thực tế trong khoảng thời gian gần đó.
+"""
 
 # Thay giá trị NaN bằng cách nội suy 10 giá trị xung quanh
 df = preprocessor.fill_missing(strategy="interpolate", neighbors=10)
@@ -74,10 +74,19 @@ nan_counts
 print("\n Tự động chuyển kiểu dữ liệu...")
 df=preprocessor.auto_convert()
 
+visualizer.df = df
+# Gọi hàm full_report()
+# print(" Báo cáo Trực quan Tự động:")
+# visualizer.full_report()
+
+"""#CHUẨN HÓA DỮ LIỆU SỐ (DataScaler)"""
+
 # Chuẩn hóa bằng MinMax Scaling (đưa về [0, 1])
 # Hàm scale sẽ tự động tìm các cột số và chuẩn hóa chúng
 print("Thực hiện MinMax Scaling cho các cột số...")
 scaler.scale(method="minmax")
+
+"""#KIỂM TRA VÀ XUẤT DỮ LIỆU"""
 
 # Kiểm tra DataFrame cuối cùng
 print(f" DataFrame cuối cùng đã xử lý :")
